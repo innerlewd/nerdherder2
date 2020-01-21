38b899a762950cf0187321f509cb4eb59b39ccc7
Seth Dousman <sdousman3@gmail.com>
2020-01-21 16:24:02 -0500

    adding material UI and componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
+import App from './component/App;'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#e8f5e9",
+      main: "#66bb6a",
+    },
+    secondary: {
+      light: "#fafafa",
+      main: "#DC143C",
+      dark: "#e0e0e0",
+    },
+  },
+});
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+    <MuiThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Butt Stuff
+      </Button>
+      </MuiThemeProvider>
+      
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.querySelector('#app'));
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#e8f5e9",
+      main: "#66bb6a",
+    },
+    secondary: {
+      light: "#fafafa",
+      main: "#DC143C",
+      dark: "#e0e0e0",
+    },
+  },
+});
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+    <MuiThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Butt Stuff
+      </Button>
+      </MuiThemeProvider>
+      
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
