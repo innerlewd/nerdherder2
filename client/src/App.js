64896c9d8a9431f298a109f4d436c97f8dd37bb4
Seth Dousman <sdousman3@gmail.com>
2020-01-21 18:59:38 -0500

    fixed file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from "@material-ui/core/styles";
-import App from './components/App;'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#e8f5e9",
+      main: "#66bb6a",
+    },
+    secondary: {
+      light: "#fafafa",
+      main: "#DC143C",
+      dark: "#e0e0e0",
+    },
+  },
+});
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+class App extends React.Component {
+  render() {
+    return (
+      <div name ='app'>
+        <MuiThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Butt Stuff
+      </Button>
+      </MuiThemeProvider>
+      
+      </div>
+    );
+  }
+}
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./App.css";
-import App from './components/App';
+import App from "./App";
 
 
 ReactDOM.render(<App />, document.querySelector('#app'));
