02ec0c8973444efa033b56555111f2c42bc0f8d0
Seth Dousman <sdousman3@gmail.com>
2020-01-21 16:52:30 -0500

    cleaned up files, connected src files to component files

--- client/src/App.js
+++ client/src/App.js
@@ -5,36 +5,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from "@material-ui/core/styles";
-import App from './component/App;'
+import App from './components/App;'
 
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#e8f5e9",
-      main: "#66bb6a",
-    },
-    secondary: {
-      light: "#fafafa",
-      main: "#DC143C",
-      dark: "#e0e0e0",
-    },
-  },
-});
-
-
-
-class App extends React.Component {
-  render() {
-    return (
-    <MuiThemeProvider theme={theme}>
-      <Button variant="contained" color="primary">
-        Butt Stuff
-      </Button>
-      </MuiThemeProvider>
-      
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.querySelector('#app'));
--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import { createMuiTheme } from "@material-ui/core/styles";
+import App from './components/App';
 
-
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#e8f5e9",
-      main: "#66bb6a",
-    },
-    secondary: {
-      light: "#fafafa",
-      main: "#DC143C",
-      dark: "#e0e0e0",
-    },
-  },
-});
-
-
-
-class App extends React.Component {
-  render() {
-    return (
-    <MuiThemeProvider theme={theme}>
-      <Button variant="contained" color="primary">
-        Butt Stuff
-      </Button>
-      </MuiThemeProvider>
-      
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.querySelector('#app'));
