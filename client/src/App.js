169e62d3336b18455886e5754b7403ecdc9218f3
Seth Dousman <sdousman3@gmail.com>
2020-01-30 15:59:16 -0500

    getting api call back in console

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
+import axios from "axios";
 
 function App() {
   return (
@@ -23,5 +24,26 @@ function App() {
     </Router>
   );
 }
+
+
+axios({
+    "method":"GET",
+    "url":"https://rawg-video-games-database.p.rapidapi.com/games",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"rawg-video-games-database.p.rapidapi.com",
+    "x-rapidapi-key":"43acb27062msh71014d2b6f89ea0p19fc0bjsn4d672d238f0f"
+    }
+    })
+    .then((response)=>{
+      console.log(response.data.results)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  
+
+
+
 
 export default App;
