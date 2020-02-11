c2219f9e757c123f68889d4971c8318a2c036841
Seth Dousman <sdousman3@gmail.com>
2020-02-10 21:08:05 -0500

    merged with frontend and backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 const fs = require('fs')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 
 
 
@@ -13,9 +14,9 @@ const gameRouter = require('./routes/game-router')
 const userRouter = require('./routes/user-router')
 
 const app = express()
-const Port = 5000 || process.env.PORT
+const Port = process.env.PORT || 5000
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -33,20 +34,19 @@ mongoose
 
 
 
-app.get('/')
 
-app.get('/logout',function(req,res){    
-    req.session.destroy(function(err){  
-        if(err){  
-            console.log(err);  
-        }  
-        else  
-        {  
-            res.redirect('/');  
-        }  
-    });  
+// app.get('/logout',function(req,res){    
+//     req.session.destroy(function(err){  
+//         if(err){  
+//             console.log(err);  
+//         }  
+//         else  
+//         {  
+//             res.redirect('/');  
+//         }  
+//     });  
 
-});  
+// });  
 
 // Passport middleware
 app.use(passport.initialize());
@@ -55,5 +55,14 @@ require("./config/passport")(passport);
 
 app.use('/api', gameRouter)
 app.use('/api/users', userRouter)
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(Port, () => console.log(`Server running on port ${Port}`))
