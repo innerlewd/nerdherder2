28425104b0242cdbe894c8b4bc05c6d82cccc147
Lance <innerlewd@gmail.com>
2020-02-10 20:00:47 -0500

    adsasda

--- server.js
+++ server.js
@@ -53,16 +53,16 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-app.use('/api/games', gameRouter)
+app.use('/api', gameRouter)
 app.use('/api/users', userRouter)
 
 //serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   });
+// }
 
 app.listen(Port, () => console.log(`Server running on port ${Port}`))
